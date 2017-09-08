6de94992803e0ac60454d169022e913a04d13b55
Aleks <aleks.woronow@yandex.ru>
2017-09-08 14:04:59 +0300

    Created todo project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { TodoStore } from './app.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [
+    TodoStore
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
